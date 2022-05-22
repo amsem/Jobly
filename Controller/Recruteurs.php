aa8657b200d7bbb03dd2cebead566aa35592af1a
C00LD3R <aomar.khelifi2001@gmail.com>
2022-05-22 21:52:16 +0100

    adding recruteur model and controller containing register, login and logout functions

---
+++ Controller/Recruteurs.php
@@ -0,0 +1,105 @@
+<?php
+
+require_once "../Model/Recruteur.php";
+require_once "../Libraries/flash.php";
+require "Users.php";
+
+class Recruteurs extends Users{
+    private $recruteurModel;
+    public function __construct(){
+        $this->recruteurModel = new Recruteur;
+    }
+
+    public function register(){
+        $data = [
+            'nom' => trim($_POST['nom']),
+            'prenom' => trim($_POST['prenom']),
+            'entreprise' => trim($_POST['entreprise']),
+            'email' => trim($_POST['email']),
+            'tel' => trim($_POST['tel'])
+
+        ];
+
+        if(empty($data['nom']) || empty($data['prenom']) || 
+        empty($data['entreprise']) || empty($data['email']) || empty($data['tel'])){
+            flash("register", "Please fill out all inputs");
+            header("Location: ../view/index.php");
+            die();
+        }
+
+        if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
+            flash("register", "Invalid email");
+            header("Location: ../view/index.php");
+            die();
+        }
+
+        if(strlen($data['password']) < 8){
+            flash("register", "Invalid password");
+            header("Location: ../view/index.php");
+            die();
+        }
+
+        if(strlen($data['tel']) < 10){
+            flash("register", "Invalid telephone number");
+            header("Location: ../view/index.php");
+            die();
+        }
+
+        if($this->recruteurModel->checkIfUserExists($data['email'])){
+            flash("register", "Username or email already taken");
+            header("Location: ../view/index.php");
+            die();
+        }
+
+        $data['password'] = password_hash($data['password'],PASSWORD_DEFAULT);
+
+        if($this->candidatModel->register($data)){
+            echo "registered";
+            // header("Location: ../view/index.php");
+        }else{
+            die("Something went wrong");
+        }
+    }
+
+    public function login(){
+        $data = [
+            'user' => $_POST['user'],
+            'password' => $_POST['password']
+        ];
+
+        if(empty($data['user']) || empty($data['password'])){
+            flash("login", "Please fill out all inputs");
+            header("Location: ../view/index.php");
+            exit();
+        }
+        
+        if($this->candidatModel->checkIfUserExists($data['user'],$data['user'])){
+            $logged = $this->candidatModel->login($data['user'],$data['password']);
+            if($logged){
+                $this->makeSession($logged,'candidateur');
+                echo "logged";
+                // header("Location: ../view/offres.php");  
+                }
+            else{
+                flash("login", "Password Incorrect");
+                header("Location: ../view/index.php");
+            }
+        }else{
+            flash("login", "No user found");
+            header("Location: ../view/index.php");
+        }
+    }
+        
+
+        
+    }
+
+    $init = new Recruteurs;
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(isset($_POST['register'])) $init->register();
+        if(isset($_POST['login'])) $init->login();
+    }else{
+        if(isset($_GET['q'])) $init->logout();
+    }
+    
+?>
---
+++ Model/Recruteur.php
@@ -0,0 +1,51 @@
+<?php
+    require_once "../Libraries/Database.php";
+    
+    class Recruteur{
+        private $db;
+        public function __construct(){
+            $this->db = new Database;
+        }
+
+        public function checkIfUserExists($email){
+            $this->db->query('SELECT * FROM recruteur WHERE email = :email');
+            $this->db->bind(':email',$email);
+            $row = $this->db->single();
+            if($this->db->rowCount() > 0){
+                return $row;
+            }else{
+                return false;
+            }
+        }
+
+        public function register($data){
+            $this->db->query('INSERT INTO recruteur (nom, prenom, entreprise, email, tel, valider) VALUES (:nom, :prenom, :entreprise, :email, :tel, :valider)');
+            $this->db->bind(':nom', $data['nom']);
+            $this->db->bind(':prenom', $data['prenom']);
+            $this->db->bind(':entreprise', $data['entreprise']);
+            $this->db->bind(':email', $data['email']);
+            $this->db->bind(':tel', $data['tel']);
+            $this->db->bind(':valider', 0);
+
+            
+            if($this->db->execute()){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        public function login($email,$password){
+            $row = $this->checkIfUserExists($email);
+            if($row == false) return false;
+            $hashedPass = $row->password;
+            if(password_verify($password,$hashedPass)){
+                return $row;
+            }else{
+                return false;
+            }
+        }
+
+    }
+
+?>
