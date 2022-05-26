c8993c2e31cf49e3b0aaf9f08d621fc3940ce5ee
C00LD3R <aomar.khelifi2001@gmail.com>
2022-05-26 06:53:59 +0100

    fixing empty search bug

--- Libraries/Search.php
+++ Libraries/Search.php
@@ -9,6 +9,9 @@
 
         public function search($term){
             $term= trim(strtolower($term));
+            if (empty($term)){
+                return "Please enter a search term";
+            }
             $this->db->query("SELECT * FROM jobs WHERE title LIKE :term or job_desc LIKE :term");
             $this->db->bind(':term','%'.$term.'%');
             $row =  $this->db->resultSet();
