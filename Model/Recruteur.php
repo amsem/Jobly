3dfa041bf6952a7e6e22a792e59ebf768904a6ef
C00LD3R <aomar.khelifi2001@gmail.com>
2022-06-29 18:18:40 +0100

    walo

--- Model/Recruteur.php
+++ Model/Recruteur.php
@@ -19,21 +19,14 @@
         }
 
         public function register($data){
-<<<<<<< HEAD
-            $this->db->query('INSERT INTO recruteur (name, family_name, company_name, email, tel, password) VALUES (:nom, :prenom, :entreprise, :email, :tel, :password)');
-=======
             $this->db->query('INSERT INTO recruteur (nom, prenom, entreprise, email, tel, password, valider) VALUES (:nom, :prenom, :entreprise, :email, :tel, :password, :valider)');
->>>>>>> 93b06163dcab9b0b997bac632323ee1bd62cfb25
             $this->db->bind(':nom', $data['nom']);
             $this->db->bind(':prenom', $data['prenom']);
             $this->db->bind(':entreprise', $data['entreprise']);
             $this->db->bind(':email', $data['email']);
             $this->db->bind(':tel', $data['tel']);
             $this->db->bind(':password', $data['password']);
-<<<<<<< HEAD
-=======
             $this->db->bind(':valider', 0);
->>>>>>> 93b06163dcab9b0b997bac632323ee1bd62cfb25
 
             
             if($this->db->execute()){
