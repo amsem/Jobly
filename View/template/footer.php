91e26483b4a49a21a04ecaa6fec8a5026944ba48
C00LD3R <aomar.khelifi2001@gmail.com>
2022-07-05 16:27:34 +0100

    footer

--- View/template/footer.php
+++ View/template/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Start -->
 <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
             <div class="container py-5">
-                <div class="row g-5">
+                <div class="row g-5" style="justify-content: space-between;">
                     <div class="col-lg-3 col-md-6">
                         <h5 class="text-white mb-4">Jobly</h5>
                         <a class="btn btn-link text-white-50" href="#aboutUs">About Us</a>
